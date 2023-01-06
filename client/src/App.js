d52b50f3c215576f3a3b636229eb2871a0097888
adriansth <soto.herrera.adrian@gmail.com>
2023-01-05 18:25:35 -0600

    build: Created pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+// styles
 import './App.css';
+// react router 
+import { Routes, Route } from 'react-router-dom';
+// components 
+import Navbar from './components/global/Navbar';
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Shop from './pages/Shop';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
 
 function App() {
      return(
-          <div>
-               Hello World
+          <div className='App'>
+               <Navbar />
+               <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/shop' element={<Shop />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/contact' element={<Contact />} />
+               </Routes>
           </div>
      );
 }
