7c305edb7bcf3e81eaa293b2b623e4d3d12a710b
adriansth <soto.herrera.adrian@gmail.com>
2023-01-09 16:09:10 -0600

    feat: Created Collection Cards at Home Page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 // components
 import Slider from '../components/home/Slider';
+import CollectionCards from '../components/home/CollectionCards';
 
 export default function Home() {
      return(
           <div>
                <Slider />
+               <CollectionCards />
           </div>
      );
 }
